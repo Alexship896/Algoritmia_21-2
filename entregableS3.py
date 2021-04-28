de13829eed9c01e4badb8e34549f967824d3c697
Alexship896 <80789251+Alexship896@users.noreply.github.com>
2021-04-28 01:17:15 -0500

    Add files via upload

---
+++ entregableS3.py
@@ -0,0 +1,25 @@
+animal = {
+    'nombre':'Cristian',
+    'colorPrimario':'Rojo',
+    'colorSecundario': 'Morado',
+    'peso':'180.5', # kg float
+    'amistoso': 'false', #boleano
+    'añosDeVida':'10', #int
+    'criado': 'si',
+    'todassusvacunas': 'No',
+    'sobrenombre': 'Cris',
+    'Noble':'si',
+    'Estaentrenado':'si'
+} 
+print(animal['nombre'])
+print(animal['peso'])
+print(type('peso'))
+print(animal['colorPrimario'])
+print(animal['colorSecundario']) 
+print(animal['amistoso'])
+print(animal['añosDeVida'])
+print(animal['sobrenombre'])
+print(animal['todassusvacunas'])
+print(animal['criado'])
+print(animal['Noble'])
+print(animal['Estaentrenado'])
