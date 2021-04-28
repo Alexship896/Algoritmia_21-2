1ffb33fc2b93899d813e67d958e437d66a39070e
Alexship896 <80789251+Alexship896@users.noreply.github.com>
2021-04-28 14:15:30 -0500

    Add files via upload

---
+++ Entregable S2.py
@@ -0,0 +1,20 @@
+# Esto es un comentario
+# Input siempre regresa una string(palabra)
+
+
+# Nombre
+nombre = "Cristian"
+print("Mi nombre es " + nombre)
+
+# Apellidos
+apellidoPaterno = "Flores"
+apellidoMaterno = "Bernal"
+apellidos = apellidoPaterno + ' ' + apellidoMaterno
+print("Mis Apellidos son: " + apellidos)
+
+# Edad
+edad = 0
+print("Edad:", edad)
+edad = int(input("Cual es tu edad? "))  # "21" -> 21
+print("Edad:", edad)
+print("Edad + 5:", edad + 5)
